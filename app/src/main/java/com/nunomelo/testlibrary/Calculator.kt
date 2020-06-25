04aae3d5f3312b12255f654cb1b505c59431f823
Nuno.melo <nuno.melo@nosi.cv>
2020-06-25 11:06:48 -0100

    Initial commit

---
+++ app/src/main/java/com/nunomelo/testlibrary/Calculator.kt
@@ -0,0 +1,32 @@
+package com.nunomelo.testlibrary
+
+
+
+object Calculator {
+
+    fun Plus(  a : Int,  b : Int) : Int{
+
+        return a + b
+    }
+
+    fun Minus(  a : Int,  b : Int) : Int{
+
+        return a - b
+    }
+
+    fun Times(  a : Int,  b : Int) : Int{
+
+        return a * b
+    }
+
+    fun Dev(  a : Int,  b : Int) : Float {
+
+        return if (b == 0) {
+            throw IllegalArgumentException("Div by Zero Error!")
+        } else {
+            (a / b).toFloat()
+
+        }
+    }
+
+}
